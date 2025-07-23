1ecccdd4d40e95cdb6f5dc60e423dfb2a2fe2a42
SchellDev <schelldev@gmail.com>
2025-07-22 23:06:37 -0300

    transformei bottomSheet em um widget. Possível avanço na lógica do image_picker

---
+++ lib/presentation/pages/widgets/custom_bottom_sheet.dart
@@ -0,0 +1,150 @@
+import 'dart:io';
+
+import 'package:desafio_final_lincetech_academy/presentation/providers/bottom_sheet_state.dart';
+import 'package:desafio_final_lincetech_academy/presentation/providers/participant_state.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../../entities/enum_transpMethod.dart';
+import '../../../entities/participant.dart';
+import '../../../l10n/app_localizations.dart';
+import '../../../use_cases/image_picker_use_cases.dart';
+import 'custom_action_button.dart';
+import 'custom_header.dart';
+import 'custom_transport_method.dart';
+import 'package:provider/provider.dart';
+
+class CustomBottomSheet extends StatefulWidget {
+  const CustomBottomSheet({super.key});
+
+  @override
+  State<CustomBottomSheet> createState() => _CustomBottomSheetState();
+}
+
+class _CustomBottomSheetState extends State<CustomBottomSheet> {
+  TextEditingController nameController = TextEditingController();
+
+  TextEditingController ageController = TextEditingController();
+
+  EnumTransportationMethod selectedTransport =
+      EnumTransportationMethod.airplane;
+
+  XFile? selectedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.9,
+      child: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: CustomHeader(
+                  text: AppLocalizations.of(context)!.addParticipantButton,
+                  size: 20,
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) => Dialog(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("Choose picture from..."),
+                            const SizedBox(height: 15),
+                            Row(
+                              spacing: 30,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CustomActionButton(
+                                  text: "Choose from camera",
+                                  onPressed: () async {
+                                    final picker = ImagePickerUseCase();
+                                    final newImage = await picker
+                                        .pickFromCamera();
+                                    if (newImage != null) {
+                                      setState(() {
+                                        selectedImage = newImage;
+                                      });
+                                    }
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                CustomActionButton(
+                                  text: "Choose from gallery",
+                                  onPressed: () async {
+                                    final picker = ImagePickerUseCase();
+                                    final newImage = await picker
+                                        .pickFromGallery();
+                                    if (newImage != null) {
+                                      setState(() {
+                                        selectedImage = newImage;
+                                      });
+                                    }
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                child: CircleAvatar(
+                  radius: 100,
+                  backgroundImage: selectedImage == null
+                      ? AssetImage("assets/images/pfp_placeholder.png")
+                      : FileImage(File(selectedImage!.path)) as ImageProvider,
+                ),
+              ),
+              CustomHeader(text: "Name"),
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: "Enter name here..."),
+              ),
+              SizedBox(height: 30),
+
+              CustomHeader(text: "Age"),
+              TextFormField(
+                controller: ageController,
+                decoration: InputDecoration(labelText: "Enter age here..."),
+              ),
+              SizedBox(height: 30),
+
+              CustomHeader(text: "Favorite Transport"),
+              CustomTranportMethod(),
+              SizedBox(height: 50),
+
+              CustomActionButton(
+                text: "Add",
+                onPressed: () {
+                  Participant p = Participant(
+                    name: nameController.text,
+                    age: int.parse(ageController.text),
+                    favoriteTransp: selectedTransport,
+                    photoPath: "",
+                  );
+                  Provider.of<ParticipantProvider>(
+                    context,
+                    listen: false,
+                  ).addParticipant(p);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
