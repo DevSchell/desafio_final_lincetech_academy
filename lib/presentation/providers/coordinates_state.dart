4946ba374a50794f5b27597fb46c689a8fa89f41
SchellDev <schelldev@gmail.com>
2025-07-29 11:14:22 -0300

    add: coordinates_state pra começar a mexer com as libs geolocator e google_maps_flutter

---
+++ lib/presentation/providers/coordinates_state.dart
@@ -0,0 +1,47 @@
+import 'package:desafio_final_lincetech_academy/entities/coordinate.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps/google_maps.dart';
+
+class CoordinatesProvider with ChangeNotifier {
+  double? latitude;
+  double? longitude;
+  String? errorMessage;
+
+  getPosition() async {
+    try{
+      Position position = await _currentPosition();
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      errorMessage = e.toString();
+    }
+  }
+
+  //Method to get the current position of the device
+  Future<Position> _currentPosition() async {
+    LocationPermission permission;
+    bool isActive = await Geolocator.isLocationServiceEnabled();
+
+    if(!isActive) {
+      return Future.error("Please enable localization services in your phone");
+    }
+
+    permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied) {
+
+      permission = await Geolocator.requestPermission();
+      if(permission == LocationPermission.denied) {
+        return Future.error("You need to enable your location services in your phone");
+      }
+    }
+
+    if(permission == LocationPermission.deniedForever) {
+      return Future.error("You must enable your device location services on your phone");
+    }
+
+    return await Geolocator.getCurrentPosition();
+
+  }
+
+}
